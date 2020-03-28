67255fd85dda44ec93b69b00387fa5397b3333a9
Zeljko Tadin <zeljko.tadin@pseudocode.agency>
2020-03-28 12:55:43 +0100

    Added Task 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Simple React example</p>
+        <p>Counter Value: {counter}</p>
+        <div>
+          <button onClick={() => setCounter(counter + 1)}>Increment</button>
+        </div>
+        <div>
+          <button onClick={() => setCounter(counter - 1)}>Decrement</button>
+        </div>
       </header>
     </div>
   );
